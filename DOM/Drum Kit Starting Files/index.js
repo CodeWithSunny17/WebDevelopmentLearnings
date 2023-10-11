8ebbec8b6657613fa01b209cdafb81ff9af9637a
Sunny Kumar <sunny274.kr@gmail.com>
2023-10-11 22:10:21 +0530

    commit 2

--- DOM/Drum Kit Starting Files/index.js
+++ DOM/Drum Kit Starting Files/index.js
@@ -1,5 +1,33 @@
 for(i=0;i<document.querySelectorAll(".drum").length;i++){
-    document.querySelectorAll(".drum").addEventListner("click", function(){
-        alert("mazza aaya!")
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        
+        if(i==0){
+            var audio = new Audio("/sounds/tom-1.mp3");
+            audio.play();
+        }
+        else if(i==1){
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+        }
+        else if(i==2){
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+        }
+        else if(i==3){
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+        }
+        else if(i==4){
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+        }
+        else if(i==5){
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+        }
+        else if(i==6){
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+        }
     })
 }
