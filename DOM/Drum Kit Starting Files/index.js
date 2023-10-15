4033cc02a3346c432792bccd1eced5638e9c7765
Sunny Kumar <sunny274.kr@gmail.com>
2023-10-15 09:41:17 +0530

    drums final commit

--- DOM/Drum Kit Starting Files/index.js
+++ DOM/Drum Kit Starting Files/index.js
@@ -1,33 +1,57 @@
-for(i=0;i<document.querySelectorAll(".drum").length;i++){
-    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+var n=document.querySelectorAll(".drum").length;
+for (i = 0; i < n; i++) {
+
+//detecting BUTTON press
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+        var button= this.innerHTML;
+        makeSound(button);
         
-        if(i==0){
-            var audio = new Audio("/sounds/tom-1.mp3");
-            audio.play();
-        }
-        else if(i==1){
-            var audio = new Audio("sounds/tom-2.mp3");
-            audio.play();
-        }
-        else if(i==2){
-            var audio = new Audio("sounds/tom-3.mp3");
-            audio.play();
-        }
-        else if(i==3){
-            var audio = new Audio("sounds/tom-4.mp3");
-            audio.play();
-        }
-        else if(i==4){
-            var audio = new Audio("sounds/tom-1.mp3");
-            audio.play();
-        }
-        else if(i==5){
-            var audio = new Audio("sounds/tom-1.mp3");
-            audio.play();
-        }
-        else if(i==6){
-            var audio = new Audio("sounds/tom-1.mp3");
-            audio.play();
-        }
+        
     })
+}
+
+
+//detecting KEYBOARD press
+document.addEventListener("keydown", function(event){
+    makeSound(event.key);
+}) 
+    
+
+
+function makeSound(key){
+    switch (key) {
+        case "w":
+            let audio1 = new Audio('sounds/tom-1.mp3');
+            audio1.play();
+            break;
+        case "a":
+            var audio2 = new Audio('sounds/tom-2.mp3');
+            audio2.play();
+            break;
+        case "s":
+            var audio3 = new Audio('sounds/tom-3.mp3');
+            audio3.play();
+            break;
+        case "d":
+            var audio4 = new Audio('sounds/tom-4.mp3');
+            audio4.play();
+            break;
+        case "j":
+            var audio5 = new Audio('sounds/snare.mp3');
+            audio5.play();
+            break;
+        case "k":
+            var audio6 = new Audio('sounds/crash.mp3');
+            audio6.play();
+            break;
+        case "l":
+            var audio7 = new Audio('sounds/kick-bass.mp3');
+            audio7.play();
+            break;
+
+
+        default: console.log(sound)
+            break;
+    }
 }
