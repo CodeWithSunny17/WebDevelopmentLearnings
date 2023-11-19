a3d8d90bb88dfb390747e81f875c0a2cb46852ae
Sunny Kumar <sunny274.kr@gmail.com>
2023-11-19 23:35:31 +0530

    keeper app commit

---
+++ React/keeper-app-part-1-starting/src/component/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const d = new Date();
+
+function Footer(){
+    return <footer>
+        <p>Copyright {d.getFullYear()}</p>
+    </footer>
+}
+
+export default Footer;
---
+++ React/keeper-app-part-1-starting/src/component/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note(){
+    return <note>
+        <div className="note">
+            <h1>title</h1>
+            <p>content</p>
+        </div>
+    </note>
+}
+
+export default Note;
